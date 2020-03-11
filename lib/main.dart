8f740d551ae7719b15c4ea613bbb5292d62bf268
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-03-11 09:01:28 -0300

    adicionado modal para incluir despesas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'components/transaction_user.dart';
+import 'dart:math';
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
+import './components/transaction_form.dart';
+import './components/transaction_list.dart';
+import 'package:expenses/models/transaction.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -12,28 +16,69 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transaction = [
+    Transaction(
+        id: 't1', title: 'Conta #01', value: 310.76, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta #02', value: 211.30, date: DateTime.now()),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transaction.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return TransactionForm(_addTransaction);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add), onPressed: () => _openTransactionFormModal(context)),
+        ],
       ),
       body: SingleChildScrollView(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-        child: Card(
-          color: Colors.blue,
-          child: Text('Gráfico'),
-          elevation: 5,
-        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
               ),
-              TransactionUser()
-            ],
-          ),
+            ),
+            TransactionList(_transaction),
+          ],
+        ),
       ),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () => _openTransactionFormModal(context)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
