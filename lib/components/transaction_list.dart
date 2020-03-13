fa842b50bc66b2f0ae98eec53ba99ddd6097d763
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-03-12 21:03:44 -0300

    adicionado MediaQuery para deixar aplicacao mais responsiva

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,55 +10,52 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: transaction.isEmpty
-          ? Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                Text(
-                  'Nenhuma transacao cadastrada!',
-                  style: Theme.of(context).textTheme.title,
+    return transaction.isEmpty
+        ? Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              Text(
+                'Nenhuma transacao cadastrada!',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(height: 20),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemCount: transaction.length,
-              itemBuilder: (ctx, index) {
-                final tr = transaction[index];
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(child: Text('R\$ ${tr.value}')),
-                      ),
-                    ),
-                    title: Text(
-                      '${tr.title}',
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(DateFormat('d MMM y').format(tr.date)),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => onRemove(tr.id),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemCount: transaction.length,
+            itemBuilder: (ctx, index) {
+              final tr = transaction[index];
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(child: Text('R\$ ${tr.value}')),
                     ),
                   ),
-                );
-              },
-            ),
-    );
+                  title: Text(
+                    '${tr.title}',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(DateFormat('d MMM y').format(tr.date)),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => onRemove(tr.id),
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -86,21 +86,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text('Despesas Pessoais'),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _openTransactionFormModal(context)),
+      ],
+    );
+
+    final availableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => _openTransactionFormModal(context)),
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionList(_transaction, _removeTransaction),
+            Container(
+              height: availableHeight * 0.3,
+              child: Chart(_recentTransactions),
+            ),
+            Container(
+              height: availableHeight * 0.7,
+              child: TransactionList(_transaction, _removeTransaction),
+            ),
           ],
         ),
       ),
